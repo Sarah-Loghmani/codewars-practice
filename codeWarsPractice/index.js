0169e82d32f750ab5e3aade9e7d5dbef2201a43e
sarah loghmani <sarah.loghmani80@gmail.com>
2022-05-21 19:50:54 +0430

    1

--- codeWarsPractice/index.js
+++ codeWarsPractice/index.js
@@ -124,5 +124,32 @@ function unusedDigits(...arr) {
     .filter((num) => true && num)
     .join("");
 }
+//*2
+// const unusedDigits = (...arr) => {
+//   var digits = arr.join();
+
+//   return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//     .filter((x) => !digits.includes(x))
+//     .join("");
+// };
 
 console.log(unusedDigits(2015, 8, 26));
+// !------------------------------
+
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+// // "What was the name of your first pet?"
+
+// "Skippy" --> "##ippy"
+
+// "Nananananananananananananananana Batman!"
+// -->
+// "####################################man!"
