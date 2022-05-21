b5f0100610d395cfcb7d826719d302dd1a47b5d7
sarah loghmani <sarah.loghmani80@gmail.com>
2022-05-21 18:34:47 +0430

    1

--- codeWarsPractice/index.js
+++ codeWarsPractice/index.js
@@ -74,4 +74,21 @@ function dontGiveMeFive(start, end) {
     array.filter((ele) => !ele.toString().includes("5")).length
   );
 }
+//*2
+// function dontGiveMeFive(start, end) {
+//   let count = 0;
+//   for (let i = start; i <= end; i++) {
+//     if (!/5/.test(i)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+//*3function dontGiveMeFive(start, end){
+//   let res = [];
+//   for (let i = start; i <= end; i++) {
+//     if (!i.toString().includes('5')) res.push(i);
+//   }
+//   return res.length;
+// }
 console.log(dontGiveMeFive(1,9));
