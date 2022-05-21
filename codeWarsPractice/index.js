85bd962fe642015014583e3d374c23e17b34827b
sarah loghmani <sarah.loghmani80@gmail.com>
2022-05-21 16:46:50 +0430

    1

--- codeWarsPractice/index.js
+++ codeWarsPractice/index.js
@@ -20,3 +20,15 @@ const descendingOrder = (num) => {
 };
 
 console.log(descendingOrder(0));
+
+// !-------------------------------------------------------------------------
+
+// Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+// spacify("hello world") // returns "h e l l o   w o r l d"
+
+function spacify(str) {
+  return str.split("").map(ele => ele +' ').join(' ');
+}
+console.log(spacify("hello world"));
+
