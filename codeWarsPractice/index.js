3195913c144a947a03d0c37111c433d12adb1d0b
sarah loghmani <sarah.loghmani80@gmail.com>
2022-05-22 18:56:17 +0430

    2

--- codeWarsPractice/index.js
+++ codeWarsPractice/index.js
@@ -271,4 +271,18 @@ function moveZeros(arr) {
   }
   return result
 }
+
+//*2
+// var moveZeros = function (arr) {
+//   return arr
+//     .filter(function (x) {
+//       return x !== 0;
+//     })
+//     .concat(
+//       arr.filter(function (x) {
+//         return x === 0;
+//       })
+//     );
+// };
+
 console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
