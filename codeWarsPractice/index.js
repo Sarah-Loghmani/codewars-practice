9a064d5d7c181e6c9f90ea0c31f019194bea986c
sarah loghmani <sarah.loghmani80@gmail.com>
2022-05-22 18:53:31 +0430

    1

--- codeWarsPractice/index.js
+++ codeWarsPractice/index.js
@@ -254,3 +254,21 @@ const solution1 = (number)=>{
 }
 
 console.log(solution1(10));
+
+// !------------------------------
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+
+function moveZeros(arr) {
+  const result = arr.filter((a) => a !== 0);
+  const firstLength = arr.length
+  const secondLength = result.length;
+  for(let i=1 ; i<=firstLength - secondLength; i++){
+      result.push(0)
+  }
+  return result
+}
+console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
