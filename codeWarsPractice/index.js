453cb7cc6b1c40e523188f2165669071186a3655
sarah loghmani <sarah.loghmani80@gmail.com>
2022-05-22 14:04:55 +0430

    1

--- codeWarsPractice/index.js
+++ codeWarsPractice/index.js
@@ -154,12 +154,12 @@ console.log(unusedDigits(2015, 8, 26));
 // -->
 // "####################################man!"
 function maskify(cc) {
-  const length= cc.length - 4;
-  const lastFourCharacter= cc.slice(-4).split('')
-  for (let i=0; i<= length; i++) {
-      lastFourCharacter.unshift('#');
+  const length = cc.length - 4;
+  const lastFourCharacter = cc.slice(-4).split("");
+  for (let i = 0; i <= length; i++) {
+    lastFourCharacter.unshift("#");
   }
-  return lastFourCharacter.join('');
+  return lastFourCharacter.join("");
 }
 //*2
 // function maskify(cc) {
@@ -185,3 +185,33 @@ console.log(maskify("4556364607935616"));
 // We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 // The input string will only consist of lower case letters and/or spaces.
+
+// function getCount(str) {
+
+//   return str.split("").filter((ele) => "aeiou".includes(ele)).length;
+// }
+
+// //*2
+// function getCount(str) {
+//   return (str.match(/[aeiou]/gi) || []).length;
+// }
+//*3
+function getCount(str) {
+  return str.replace(/[^aeiou]/gi, "").length;
+}
+
+
+console.log(getCount("abracadabra"));
+
+// !------------------------------
+
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+// Based on: http://oj.leetcode.com/problems/two-sum/
+
+// twoSum [1, 2, 3] 4 === (0, 2)
+
