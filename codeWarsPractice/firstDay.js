f4907096fdb75756b1489e8ff32f331cc136c6d1
sarah loghmani <sarah.loghmani80@gmail.com>
2022-07-25 16:14:24 +0430

    2

--- codeWarsPractice/firstDay.js
+++ codeWarsPractice/firstDay.js
@@ -1,11 +1,6 @@
 // https://www.codewars.com/kata/571edea4b625edcb51000d8e
 // misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the uppercase and lowercase letters are different).
-
 // When you have finished the work, click "Run Tests" to see if your code is working properly.
-
-// misson 2: After misson 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2,answer3
-
-// If everything is right, click "Submit" again to submit your code pass this kata.
 
 var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
 function Dad(){
@@ -22,3 +17,34 @@ function banana(){
 }
 console.log(Dad(),Bee(), banana())
 
+
+// https://www.codewars.com/kata/571edd157e8954bab500032d/train/javascript
+// I've written five function equal1,equal2,equal3,equal4,equal5, defines six global variables v1 v2 v3 v4 v5 v6, every function has two local variables a,b, please set the appropriate value for the two variables(select from v1--v6), making these function return value equal to 100. the function equal1 is completed, please refer to this example to complete the following functions.
+
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+function equal1(){
+  var a=v1   
+  var b=v1   
+  return a+b;
+}
+//Please refer to the example above to complete the following functions
+function equal2(){
+  var a= v2  //set number value to a
+  var b= v4  //set number value to b
+  return a-b;
+}
+function equal3(){
+  var a=  v1 //set number value to a
+  var b=  v5 //set number value to b
+  return a*b;
+}
+function equal4(){
+  var a= v4  //set number value to a
+  var b= v5  //set number value to b
+  return a/b;
+}
+function equal5(){
+  var a=   //set number value to a
+  var b=   //set number value to b
+  return a%b;
+}
