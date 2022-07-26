a160889bd52a768d0043de807636aaf331a80b8c
sarah loghmani <sarah.loghmani80@gmail.com>
2022-07-26 19:12:36 +0430

    1

--- codeWarsPractice/firstDay.js
+++ codeWarsPractice/firstDay.js
@@ -91,3 +91,11 @@ function animal(obj) {
   return `This ${obj.color} ${obj["name"]} has ${obj.legs} legs.`;
 }
 
+// ternary operator
+function oddEven(n) {
+  return n % 2 == 1 ? "odd number" : "even number";
+}
+function oldYoung(age) {
+  return age < 16 ? "children" : age < 50 ? "young man" : "old man";
+}
+
