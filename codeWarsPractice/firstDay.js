0b76581a9ea620ef0fa17353ded71b0c465d8a01
sarah loghmani <sarah.loghmani80@gmail.com>
2022-07-26 19:14:21 +0430

    6

--- codeWarsPractice/firstDay.js
+++ codeWarsPractice/firstDay.js
@@ -99,3 +99,21 @@ function oldYoung(age) {
   return age < 16 ? "children" : age < 50 ? "young man" : "old man";
 }
 
+//switch
+// switch is the keyword, n is the variable to switch, case 1 means when n===1, following the ":",you can add some executable statement, with keyword break as termination(if you forgot "break", code will running with case 2, case 3...until a "break" appear),default like else in the if..else statement.
+// In some cases, use the switch statement to look more clearly than the if..else statement.
+switch(n){
+  case 1:  
+    //code block
+    break;
+  case 2:
+    //code block
+    break;
+  case 3:
+    //code block
+    break;
+
+  default:       //default is optional, sometimes it can be omitted
+    //code block
+                 //The last one does not need break
+}
