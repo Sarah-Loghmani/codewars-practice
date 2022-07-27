2d8aba1f337bf3cb97c1b4970f2f0670e62edcfd
sarah loghmani <sarah.loghmani80@gmail.com>
2022-07-27 18:35:52 +0430

    2

--- codeWarsPractice/3thDay.js
+++ codeWarsPractice/3thDay.js
@@ -54,10 +54,14 @@ function sum1_100() {
 
 // Coding in function pickIt, function accept 1 parameter:arr, it's a number array, we need traverse arr by using for loop, if element is odd number, push it to array odd, if it's a even number, push it to array even.
 
+// https://www.codewars.com/kata/5721a78c283129e416000999/train/javascript
 function pickIt(arr) {
   var odd = [],
     even = [];
-  //coding here
+
+  for(let i=0; i <= arr.length-1; i++) {
+    arr[i]%2===0 ? even.push(arr[i]) : odd.push(arr[i]) ;
+  }
 
   return [odd, even];
 }
