35d763e4f35725cbe12305be8e1a7b4128d0b3e1
sarah loghmani <sarah.loghmani80@gmail.com>
2022-07-27 16:18:10 +0430

    4

---
+++ codeWarsPractice/ninja.js/ninja.js
@@ -0,0 +1,8 @@
+// string methods
+let str= "this is a example of a string"
+
+// let result = str.lastIndexOf("a")
+// let result = str.slice(0, 8)
+let result = str.substr(2, 5)
+
+console.log(result)
